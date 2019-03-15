3c8726fe69903f75851538714fea0ca1835cd86c
Chinedu Arinze <c.s.arinze@gmail.com>
2019-03-15 13:05:29 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -28,8 +28,8 @@ def won? (board)
     if (position_1 == "X" && position_2 == "X" && position_3 == "X") || (position_1 == "O" && position_2 == "O" && position_3 == "O")
        return win_combo
     end
+    false
   end
-  false
 end
 
 def full? (board)
